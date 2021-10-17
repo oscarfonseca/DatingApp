37c9dcaf967dc5bcc1f848080e51346060d94936
Oscar Fonseca <oscar.fonseca@grollmus.de>
2021-10-17 10:28:20 +0200

    Create ApiBaseController
    It reduces code duplication because all the
     controllers share this functionality

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -7,9 +7,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : ApiBaseController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
