f3ac68b545eeca9d4ca6f9b501cc5780abaf43a9
Oscar Fonseca <oscar.e.fonseca@gmail.com>
2021-10-24 09:24:26 +0200

    Add Authentication
    The user logs in with user name and password
    It gets a token from the Controller
    From that point on the user has only to provide
    the token along with any request to be authorized

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,9 +17,11 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsersAsync() => await _context.Users.ToListAsync();
 
         [HttpGet("{id}")]
+        [Authorize]
         public async Task<ActionResult<AppUser>> GetUser(int id) => await _context.Users.FindAsync(id);
     }
 }
